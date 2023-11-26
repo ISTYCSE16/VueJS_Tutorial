716260b21ddcb2e3bcaa8611d1fcb48d9a616657
ISTYCSE16 <fatinfaiazisty@gmail.com>
2023-11-26 18:13:39 +0600

    "Some Event Listeners and Passing Methods In HTML"

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-const app = Vue.createApp()
+const app = Vue.createApp({
+    data() {
+        return {
+            name: "Walter White",
+            profession: "Teacher",
+            age: 55,
+            showData: true
+        }
+    },
+
+    methods: {
+        increaseAge() {
+            this.age++
+            console.log(this.age)
+        },
+        decreaseAge() {
+            this.age--
+            console.log("Decrease Age")
+        },
+        toggleShowData() {
+            this.showData = !this.showData
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type, data);
+        }
+    }
+})
 
 app.mount('#app')
